paulgibbs/BuddyPress

commit 34b51990dddb1ed0c5a952b815ccf08e978e124e

Update bp_core_is_user_spammer() and bp_core_is_user_deleted() functions to use get_userdata() to take advantage of WP object cache.
git-svn-id: https://buddypress.svn.wordpress.org/trunk@4592 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-members/bp-members-functions.php
@@ -481,12 +481,28 @@ function bp_core_get_total_member_count() {
 function bp_core_is_user_spammer( $user_id ) {
 	global $wpdb;
 
-	if ( is_multisite() )
-		$is_spammer = (int) $wpdb->get_var( $wpdb->prepare( "SELECT spam FROM $wpdb->users WHERE ID = %d", $user_id ) );
-	else
-		$is_spammer = (int) $wpdb->get_var( $wpdb->prepare( "SELECT user_status FROM $wpdb->users WHERE ID = %d", $user_id ) );
+	// Assume user is not spam
+	$is_spammer = false;
 
-	return apply_filters( 'bp_core_is_user_spammer', $is_spammer );
+	// Get user data
+	$user = get_userdata( $user_id );
+
+	// No user found
+	if ( empty( $user ) ) {
+		$is_spammer = false;
+		
+	// User found
+	} else {
+
+		// Check if spam
+		if ( !empty( $user->spam ) )
+			$is_spammer = true;
+
+		if ( 'spam' == $user->user_status )
+			$is_spammer = true;
+	}
+
+	return apply_filters( 'bp_core_is_user_spammer', (bool) $is_spammer );
 }
 
 /**
@@ -499,7 +515,29 @@ function bp_core_is_user_spammer( $user_id ) {
 function bp_core_is_user_deleted( $user_id ) {
 	global $wpdb;
 
-	return apply_filters( 'bp_core_is_user_spammer', (int) $wpdb->get_var( $wpdb->prepare( "SELECT deleted FROM $wpdb->users WHERE ID = %d", $user_id ) ) );
+	// Assume user is not deleted
+	$is_deleted = false;
+
+	// Get user data
+	$user = get_userdata( $user_id );
+
+	// No user found
+	if ( empty( $user ) ) {
+		$is_deleted = true;
+		
+	// User found
+	} else {
+
+		// Check if deleted
+		if ( !empty( $user->deleted ) )
+			$is_deleted = true;
+
+		if ( 'deleted' == $user->user_status )
+			$is_deleted = true;
+
+	}
+
+	return apply_filters( 'bp_core_is_user_deleted', (bool) $is_deleted );
 }
 
 /**

commit 2f4ccd3da69d98de0e98ab27882f0a6775f8cb2b

Fix regression in user status checks, introduced in r4592. Blame johnjamesjacoby. Props johnjamesjacoby.
git-svn-id: https://buddypress.svn.wordpress.org/trunk@5036 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-members/bp-members-functions.php
@@ -517,7 +517,7 @@ function bp_core_is_user_spammer( $user_id = 0 ) {
 		if ( !empty( $user->spam ) )
 			$is_spammer = true;
 
-		if ( 'spam' == $user->user_status )
+		if ( 1 == $user->user_status )
 			$is_spammer = true;
 	}
 
@@ -555,7 +555,7 @@ function bp_core_is_user_deleted( $user_id = 0 ) {
 		if ( !empty( $user->deleted ) )
 			$is_deleted = true;
 
-		if ( 'deleted' == $user->user_status )
+		if ( 2 == $user->user_status )
 			$is_deleted = true;
 
 	}