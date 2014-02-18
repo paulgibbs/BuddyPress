paulgibbs/BuddyPress

commit e36fe8eb6c54338dc2e6f9c5ed1690a3f0d4cc7d

Use the correct function to get notification unread count for buddybar
Fixes #5403

Props lenasterg

git-svn-id: https://buddypress.svn.wordpress.org/trunk@7913 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-notifications/bp-notifications-buddybar.php
@@ -27,8 +27,8 @@ function bp_notifications_buddybar_menu() {
 	echo '<li id="bp-adminbar-notifications-menu"><a href="' . esc_url( bp_loggedin_user_domain() ) . '">';
 	_e( 'Notifications', 'buddypress' );
 
-	if ( $notifications = bp_notifications_get_notifications_for_user( bp_loggedin_user_id() ) ) : ?>
-		<span><?php echo number_format_i18n( $notifications ); ?></span>
+	if ( $notification_count = bp_notifications_get_unread_notification_count( bp_loggedin_user_id() ) ) : ?>
+		<span><?php echo bp_core_number_format( $notification_count ); ?></span>
 	<?php
 	endif;
 

commit 4220afe92548d9ff8f2e135eda365fa27d1ea959

Fix Buddybar notification formatting bug introduced in r7913
Fixes #5403

git-svn-id: https://buddypress.svn.wordpress.org/trunk@7920 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-notifications/bp-notifications-buddybar.php
@@ -35,7 +35,7 @@ function bp_notifications_buddybar_menu() {
 	echo '</a>';
 	echo '<ul>';
 
-	if ( $notifications ) {
+	if ( $notifications = bp_notifications_get_notifications_for_user( bp_loggedin_user_id() ) ) {
 		$counter = 0;
 		for ( $i = 0, $count = count( $notifications ); $i < $count; ++$i ) {
 			$alt = ( 0 == $counter % 2 ) ? ' class="alt"' : ''; ?>