3298513e947dfa42d1066596deba49aa3465a3ce
Paul Gibbs <paul@byotos.com>
2014-07-19 18:39:10 +0100

    Prime member suggestions from user's Friends

--- src/bp-activity/bp-activity-cssjs.php
+++ src/bp-activity/bp-activity-cssjs.php
@@ -23,6 +23,9 @@ function bp_activity_mentions_script() {
 	$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 	wp_enqueue_script( 'bp-mentions', buddypress()->plugin_url . "bp-activity/js/mentions{$min}.js", array( 'jquery', 'jquery-atwho' ), bp_get_version(), true );
 	wp_enqueue_style( 'bp-mentions-css', buddypress()->plugin_url . "bp-activity/css/mentions{$min}.css", array(), bp_get_version() );
+
+	// Print a list of the current user's friends to the page for quicker @mentions lookups.
+	do_action( 'bp_activity_mentions_prime_results' );
 }
 add_action( 'bp_enqueue_scripts', 'bp_activity_mentions_script' );
 
@@ -47,5 +50,8 @@ function bp_activity_mentions_dashboard_script() {
 	$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 	wp_enqueue_script( 'bp-mentions', buddypress()->plugin_url . "bp-activity/js/mentions{$min}.js", array( 'jquery', 'jquery-atwho' ), bp_get_version(), false );
 	wp_enqueue_style( 'bp-mentions-css', buddypress()->plugin_url . "bp-activity/css/mentions{$min}.css", array(), bp_get_version() );
+
+	// Print a list of the current user's friends to the page for quicker @mentions lookups.
+	do_action( 'bp_activity_mentions_prime_results' );
 }
 add_action( 'bp_admin_enqueue_scripts', 'bp_activity_mentions_dashboard_script' );
