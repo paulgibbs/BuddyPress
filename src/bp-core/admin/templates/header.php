c49952d4bbfd87a21c127a7ab30442ab5e6406d9
Paul Gibbs <paul@byotos.com>
2014-12-02 10:44:54 +0000

    Another iteration on basic header placement

--- src/bp-core/admin/templates/header.php
+++ src/bp-core/admin/templates/header.php
@@ -3,22 +3,23 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 
-<div class="bpa-header bpa-width-full clearfix">
-	<p class="bpa-header-logo" aria-hidden="true">
-		<span class="screen-reader-text"><?php _e( 'BuddyPress', 'buddypress' ); ?></span>
-	</p>
+<div class="bpa-header clearfix">
+	<nav role="navigation">
+		<ul class="bpa-nav">
+			<li class="bpa-header-logo">
+				<a href="#"><span class="screen-reader-text"><?php _e( 'BuddyPress', 'buddypress' ); ?></span></a>
+			</li>
 
-	<?php
-	$nav = bp_core_get_admin_tabs();
-	if ( empty( $nav ) ) {
-		return;
-	}
-	?>
+			<?php
+			$nav = bp_core_get_admin_tabs();
+			if ( ! empty( $nav ) ) : ?>
+				<?php foreach ( $nav as $item ) : ?>
+					<li>
+						<a href="<?php echo esc_url( $item['href'] ); ?>"><?php echo esc_html( $item['name'] ); ?></a>
+					</li>
+				<?php endforeach; ?>
+			<?php endif; ?>
 
-	<ul class="bpa-nav" role="navigation">
-		<?php foreach ( $nav as $item ) : ?>
-			<li><a href="<?php echo esc_url( $item['href'] ); ?>"><?php echo esc_html( $item['name'] ); ?></a></li>
-		<?php endforeach; ?>
-	</ul><!-- .bpa-nav -->
-
+		</ul><!-- .bpa-nav -->
+	</nav>
 </div><!-- .bpa-header -->
