b5ac6b6c0f006cdaa7ff068b240bcd6c0760cc8a
Paul Gibbs <paul@byotos.com>
2014-12-07 20:18:46 +0000

    More placeholder content

--- src/bp-core/admin/templates/dashboard.php
+++ src/bp-core/admin/templates/dashboard.php
@@ -9,15 +9,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	<h1>BuddyPress helps you run any kind of social network on your WordPress, with member profiles, activity streams, user groups, messaging, and&nbsp;more.</h1>
 </div>
 
-<div class="bpa-cards">
-	<article class="bpa-card">
-		<h3>Getting You Started</h3>
-		<p>We&#8217;ve activated our two most popular modules so you can start enjoying BuddyPress immediately; Activity Streams and Community Profiles.
-		<p><strong>Activity Streams</strong> give your members a central place to discuss and comment on what other members are doing on your site. Activity Streams also provide Twitter-style @mentions and favouriting.</p>
-		<p><strong>Extended Profiles</strong> are fully editadble profile field that allow you to define the fields that your members will use to describe themselves. Tailor profile fields to suit your audience.</p>
-		<p>Here are quick introductions to these features; they only take a few minutes to learn, so you'll be up and ready in no time!</p>
-	</article>
-</div>
+<article class="bpa-card">
+	<h3>Getting You Started</h3>
+	<p>We&#8217;ve activated our two most popular modules so you can start enjoying BuddyPress immediately; Activity Streams and Community Profiles.
+	<p><strong>Activity Streams</strong> give your members a central place to discuss and comment on what other members are doing on your site. Activity Streams also provide Twitter-style @mentions and favouriting.</p>
+	<p><strong>Extended Profiles</strong> are fully editadble profile field that allow you to define the fields that your members will use to describe themselves. Tailor profile fields to suit your audience.</p>
+	<p>Here are quick introductions to these features; they only take a few minutes to learn, so you'll be up and ready in no time! Once you're up to speed, be sure to check out the other modules that BuddyPress offers.</p>
+</article>
 
 <div class="bpa-buttons">
 	<div class="bpa-button-major bpa-button-activity">
@@ -40,3 +38,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 		</a>
 	</div>
 </div>
+
+<article class="bpa-card">
+	<h3>Support &amp; Docs</h3>
+	<p>While exploring BuddyPress, if you have a question you can't find an answer to, the best place to ask is our community-driven support forums.</p>
+</article>
