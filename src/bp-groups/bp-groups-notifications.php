paulgibbs/BuddyPress

commit 0fcd29689b8e938e1a0ef14bfd7a97c204693a0e

Introduce Member Types API.
BuddyPress member types are akin to WordPress's custom post types. Developers
can use `bp_register_member_type()` to register their types with BuddyPress,
and BP will automatically provide a number of pieces of functionality:

* Mechanisms for storing and fetching member types (as a WP taxonomy term), with full cache support.
* A 'member_type' argument for the `bp_has_members()`/`BP_User_Query` stack, which allows filtering member loops by member type.
* Admin UI for changing member types on Dashboard > Users > Community Profile (appears when member types have been registered).

We'll continue to build out more core member type functionality in future
versions of BuddyPress. In the meantime, this is a good starting point for BP
site implementations to have a shared infrastructure for storing and retrieving
this data.

See #6006.

git-svn-id: https://buddypress.svn.wordpress.org/trunk@9210 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-members/bp-members-admin.php
@@ -186,6 +186,9 @@ class BP_Members_Admin {
 		// Add user row actions for single site
 		add_filter( 'user_row_actions', array( $this, 'row_actions' ), 10, 2 );
 
+		// Process changes to member type.
+		add_action( 'bp_members_admin_load', array( $this, 'process_member_type_update' ) );
+
 		/** Signups ***********************************************************/
 
 		if ( is_admin() ) {
@@ -719,6 +722,19 @@ class BP_Members_Admin {
 				sanitize_key( $this->stats_metabox->priority )
 			);
 
+			// Member Type metabox. Only added if member types have been registered.
+			$member_types = bp_get_member_types();
+			if ( ! empty( $member_types ) ) {
+				add_meta_box(
+					'bp_members_admin_member_type',
+					_x( 'Member Type', 'members user-admin edit screen', 'buddypress' ),
+					array( $this, 'user_admin_member_type_metabox' ),
+					get_current_screen()->id,
+					'side',
+					'core'
+				);
+			}
+
 			/**
 			 * Custom metabox ?
 			 * Plugins can restrict metabox to "bp_moderate" admins checking
@@ -995,6 +1011,75 @@ class BP_Members_Admin {
 		</ul>
 
 		<?php
+	}
+
+	/**
+	 * Render the Member Type metabox.
+	 *
+	 * @since BuddyPress (2.2.0)
+	 * @access public
+	 *
+	 * @param WP_User $user The WP_User object to be edited.
+	 */
+	public function user_admin_member_type_metabox( $user = null ) {
+
+		// Bail if no user ID.
+		if ( empty( $user->ID ) ) {
+			return;
+		}
+
+		$types = bp_get_member_types( array(), 'objects' );
+		$current_type = bp_get_member_type( $user->ID );
+
+		/* translators: no option picked in select box */
+		$null_option = __( '----', 'buddypress' );
+
+		?>
+		<select name="bp-members-profile-member-type">
+			<option value="" <?php selected( '', $current_type ); ?>><?php esc_attr_e( $null_option ) ?></option>
+			<?php foreach ( $types as $type ) : ?>
+				<option value="<?php echo esc_attr( $type->name ) ?>" <?php selected( $type->name, $current_type ) ?>><?php echo esc_html( $type->labels['singular_name'] ) ?></option>
+			<?php endforeach; ?>
+		</select>
+
+		<?php
+
+		wp_nonce_field( 'bp-member-type-change-' . $user->ID, 'bp-member-type-nonce' );
+	}
+
+	/**
+	 * Process changes from the Member Type metabox.
+	 *
+	 * @since BuddyPress (2.2.0)
+	 * @access public
+	 */
+	public function process_member_type_update() {
+		if ( ! isset( $_POST['bp-member-type-nonce'] ) || ! isset( $_POST['bp-members-profile-member-type'] ) ) {
+			return;
+		}
+
+		$user_id = $this->get_user_id();
+
+		check_admin_referer( 'bp-member-type-change-' . $user_id, 'bp-member-type-nonce' );
+
+		// Permission check.
+		if ( ! current_user_can( 'bp_moderate' ) && $user_id != bp_loggedin_user_id() ) {
+			return;
+		}
+
+		// Member type string must either reference a valid member type, or be empty.
+		$member_type = stripslashes( $_POST['bp-members-profile-member-type'] );
+		if ( ! empty( $member_type ) && ! bp_get_member_type_object( $member_type ) ) {
+			return;
+		}
+
+		/*
+		 * If an invalid member type is passed, someone's doing something
+		 * fishy with the POST request, so we can fail silently.
+		 */
+		if ( bp_set_member_type( $user_id, $member_type ) ) {
+			// @todo Success messages can't be posted because other stuff happens on the pageload.
+		}
 	}
 
 	/**

src/bp-core/bp-core-actions.php
@@ -65,6 +65,7 @@ add_action( 'bp_loaded', 'bp_register_theme_directory', 14 );
  *                                                   v---Load order
  */
 add_action( 'bp_init', 'bp_core_set_uri_globals',    2  );
+add_action( 'bp_init', 'bp_register_taxonomies',     3  );
 add_action( 'bp_init', 'bp_setup_globals',           4  );
 add_action( 'bp_init', 'bp_setup_canonical_stack',   5  );
 add_action( 'bp_init', 'bp_setup_nav',               6  );

src/bp-core/bp-core-classes.php
@@ -37,6 +37,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
  *                                              that should be returned. When this parameter is passed, it will
  *                                              override all others; BP User objects will be constructed using these
  *                                              IDs only. Default: false.
+ *     @type array|string      $member_type     Array or comma-separated list of member types to limit results to.
  *     @type string|bool       $meta_key        Limit results to users that have usermeta associated with this meta_key.
  *                                              Usually used with $meta_value. Default: false.
  *     @type string|bool       $meta_value      When used with $meta_key, limits results to users whose usermeta value
@@ -164,6 +165,7 @@ class BP_User_Query {
 				'include'         => false,
 				'exclude'         => false,
 				'user_ids'        => false,
+				'member_type'     => '',
 				'meta_key'        => false,
 				'meta_value'      => false,
 				'xprofile_query'  => false,
@@ -399,6 +401,46 @@ class BP_User_Query {
 				$search_terms_nospace,
 				$search_terms_space
 			);
+		}
+
+		// Member type.
+		if ( ! empty( $member_type ) ) {
+			$member_types = array();
+
+			if ( ! is_array( $member_type ) ) {
+				$member_type = preg_split( '/[,\s+]/', $member_type );
+			}
+
+			foreach ( $member_type as $mt ) {
+				if ( ! bp_get_member_type_object( $mt ) ) {
+					continue;
+				}
+
+				$member_types[] = $mt;
+			}
+
+			if ( ! empty( $member_types ) ) {
+				$member_type_tq = new WP_Tax_Query( array(
+					array(
+						'taxonomy' => 'bp_member_type',
+						'field'    => 'name',
+						'operator' => 'IN',
+						'terms'    => $member_types,
+					),
+				) );
+
+				// Switch to the root blog, where member type taxonomies live.
+				switch_to_blog( bp_get_root_blog_id() );
+
+				$member_type_sql_clauses = $member_type_tq->get_sql( 'u', $this->uid_name );
+				restore_current_blog();
+
+
+				// Grab the first term_relationships clause and convert to a subquery.
+				if ( preg_match( '/' . $wpdb->term_relationships . '\.term_taxonomy_id.*/', $member_type_sql_clauses['where'], $matches ) ) {
+					$sql['where']['member_type'] = "u.{$this->uid_name} IN ( SELECT object_id FROM $wpdb->term_relationships WHERE {$matches[0]} )";
+				}
+			}
 		}
 
 		// 'meta_key', 'meta_value' allow usermeta search

src/bp-core/bp-core-dependency.php
@@ -39,6 +39,15 @@ function bp_setup_canonical_stack() {
 }
 
 /**
+ * Fire the 'bp_register_taxonomies' action, where plugins should register taxonomies.
+ *
+ * @since BuddyPress (2.2.0)
+ */
+function bp_register_taxonomies() {
+	do_action( 'bp_register_taxonomies' );
+}
+
+/**
  * Fire the 'bp_setup_globals' action, where plugins should initialize global settings.
  */
 function bp_setup_globals() {

src/bp-core/bp-core-taxonomy.php
@@ -1,0 +1,73 @@
+<?php
+
+/**
+ * BuddyPress taxonomy functions.
+ *
+ * Most BuddyPress taxonomy functions are wrappers for their WordPress counterparts.
+ * Because BuddyPress can be activated in various ways in a network environment, we
+ * must switch to the root blog before using the WP functions.
+ *
+ * @since BuddyPress (2.2.0)
+ */
+
+/**
+ * Register our default taxonomies.
+ *
+ * @since BuddyPress (2.2.0)
+ */
+function bp_register_default_taxonomies() {
+	// Member Type.
+	register_taxonomy( 'bp_member_type', 'user', array(
+		'public' => false,
+	) );
+}
+add_action( 'bp_register_taxonomies', 'bp_register_default_taxonomies' );
+
+/**
+ * Set taxonomy terms on a BuddyPress object.
+ *
+ * @since BuddyPress (2.2.0)
+ *
+ * @see wp_set_object_terms() for a full description of function and parameters.
+ *
+ * @param int          $object_id Object ID.
+ * @param string|array $terms     Term or terms to set.
+ * @param string       $taxonomy  Taxonomy name.
+ * @param bool         $append    Optional. True to append terms to existing terms. Default: false.
+ * @return array Array of term taxonomy IDs.
+ */
+function bp_set_object_terms( $object_id, $terms, $taxonomy, $append = false ) {
+	if ( ! bp_is_root_blog() ) {
+		switch_to_blog( bp_get_root_blog_id() );
+	}
+
+	$retval = wp_set_object_terms( $object_id, $terms, $taxonomy, $append );
+
+	restore_current_blog();
+
+	return $retval;
+}
+
+/**
+ * Get taxonomy terms for a BuddyPress object.
+ *
+ * @since BuddyPress (2.2.0)
+ *
+ * @see wp_get_object_terms() for a full description of function and parameters.
+ *
+ * @param int|array    $object_ids ID or IDs of objects.
+ * @param string|array $taxonomies Name or names of taxonomies to match.
+ * @param array        $args       See {@see wp_get_object_terms()}.
+ * @return array
+ */
+function bp_get_object_terms( $object_ids, $taxonomies, $args = array() ) {
+	if ( ! bp_is_root_blog() ) {
+		switch_to_blog( bp_get_root_blog_id() );
+	}
+
+	$retval = wp_get_object_terms( $object_ids, $taxonomies, $args );
+
+	restore_current_blog();
+
+	return $retval;
+}

src/bp-loader.php
@@ -432,6 +432,7 @@ class BuddyPress {
 		require( $this->plugin_dir . 'bp-core/bp-core-update.php'     );
 		require( $this->plugin_dir . 'bp-core/bp-core-options.php'    );
 		require( $this->plugin_dir . 'bp-core/bp-core-classes.php'    );
+		require( $this->plugin_dir . 'bp-core/bp-core-taxonomy.php'   );
 		require( $this->plugin_dir . 'bp-core/bp-core-filters.php'    );
 		require( $this->plugin_dir . 'bp-core/bp-core-avatars.php'    );
 		require( $this->plugin_dir . 'bp-core/bp-core-widgets.php'    );

src/bp-members/bp-members-cache.php
@@ -1,0 +1,49 @@
+<?php
+
+/**
+ * Caching functions specific to BuddyPress Members.
+ *
+ * @since BuddyPress (2.2.0)
+ */
+
+/**
+ * Pre-fetch member type data when initializing a Members loop.
+ *
+ * @since BuddyPress (2.2.0)
+ *
+ * @param BP_User_Query $bp_user_query BP_User_Query object.
+ */
+function bp_members_prefetch_member_type( BP_User_Query $bp_user_query ) {
+	$uncached_member_ids = bp_get_non_cached_ids( $bp_user_query->user_ids, 'bp_member_type' );
+
+	$member_types = bp_get_object_terms( $uncached_member_ids, 'bp_member_type', array(
+		'fields' => 'all_with_object_id',
+	) );
+
+	$cached_member_ids = array();
+	foreach ( $member_types as $member_type ) {
+		wp_cache_set( $member_type->object_id, $member_type->name, 'bp_member_type' );
+		$cached_member_ids[] = $member_type->object_id;
+	}
+
+	// Cache an empty value for users with no type.
+	foreach ( array_diff( $uncached_member_ids, $cached_member_ids ) as $no_type_id ) {
+		wp_cache_set( $no_type_id, '', 'bp_member_type' );
+	}
+}
+add_action( 'bp_user_query_populate_extras', 'bp_members_prefetch_member_type' );
+
+/**
+ * Clear the member_type cache for a user.
+ *
+ * Called when the user is deleted or marked as spam.
+ *
+ * @since BuddyPres (2.2.0)
+ *
+ * @param int $user_id ID of the deleted user.
+ */
+function bp_members_clear_member_type_cache( $user_id ) {
+	wp_cache_delete( $user_id, 'bp_member_type' );
+}
+add_action( 'wpmu_delete_user', 'bp_members_clear_member_type_cache' );
+add_action( 'delete_user', 'bp_members_clear_member_type_cache' );

src/bp-members/bp-members-functions.php
@@ -78,19 +78,18 @@ add_action( 'bp_setup_globals', 'bp_core_define_slugs', 11 );
  * @param array $args {
  *     Array of arguments. All are optional. See {@link BP_User_Query} for
  *     a more complete description of arguments.
- *     @type string $type Sort order. Default: 'active'.
- *     @type int $user_id Limit results to friends of a user. Default: false.
- *     @type mixed $exclude IDs to exclude from results. Default: false.
- *     @type string $search_terms Limit to users matching search terms. Default: false.
- *     @type string $meta_key Limit to users with a meta_key. Default: false.
- *     @type string $meta_value Limit to users with a meta_value (with
- *           meta_key). Default: false.
+ *     @type string       $type            Sort order. Default: 'active'.
+ *     @type int          $user_id         Limit results to friends of a user. Default: false.
+ *     @type mixed        $exclude         IDs to exclude from results. Default: false.
+ *     @type string       $search_terms    Limit to users matching search terms. Default: false.
+ *     @type string       $meta_key        Limit to users with a meta_key. Default: false.
+ *     @type string       $meta_value      Limit to users with a meta_value (with meta_key). Default: false.
+ *     @type array|string $member_type     Array or comma-separated string of member types.
  *     @type mixed $include Limit results by user IDs. Default: false.
- *     @type int $per_page Results per page. Default: 20.
- *     @type int $page Page of results. Default: 1.
- *     @type bool $populate_extras Fetch optional extras. Default: true.
- *     @type string|bool $count_total How to do total user count.
- *           Default: 'count_query'.
+ *     @type int          $per_page        Results per page. Default: 20.
+ *     @type int          $page            Page of results. Default: 1.
+ *     @type bool         $populate_extras Fetch optional extras. Default: true.
+ *     @type string|bool  $count_total     How to do total user count. Default: 'count_query'.
  * }
  * @return array
  */
@@ -104,6 +103,7 @@ function bp_core_get_users( $args = '' ) {
 		'search_terms'    => false,        // Limit to users that match these search terms
 		'meta_key'        => false,        // Limit to users who have this piece of usermeta
 		'meta_value'      => false,        // With meta_key, limit to users where usermeta matches this value
+		'member_type'     => '',
 		'include'         => false,        // Pass comma separated list of user_ids to limit to only these users
 		'per_page'        => 20,           // The number of results to return per page
 		'page'            => 1,            // The page to return if limiting per page
@@ -2148,3 +2148,219 @@ function bp_live_spammer_login_error() {
 	add_action( 'login_head', 'wp_shake_js', 12 );
 }
 add_action( 'login_form_bp-spam', 'bp_live_spammer_login_error' );
+
+/** Member Types *************************************************************/
+
+/**
+ * Register a member type.
+ *
+ * @since BuddyPress (2.2.0)
+ *
+ * @param string $member_type Unique string identifier for the member type.
+ * @param array  $args {
+ *     Array of arguments describing the member type.
+ *
+ *     @type array $labels {
+ *         Array of labels to use in various parts of the interface.
+ *
+ *         @type string $name          Default name. Should typically be plural.
+ *         @type string $singular_name Singular name.
+ *     }
+ * }
+ * @return object|WP_Error Member type object on success, WP_Error object on failure.
+ */
+function bp_register_member_type( $member_type, $args = array() ) {
+	$bp = buddypress();
+
+	if ( isset( $bp->members->types[ $member_type ] ) ) {
+		return new WP_Error( 'bp_member_type_exists', __( 'Member type already exists.', 'buddypress' ), $member_type );
+	}
+
+	$r = bp_parse_args( $args, array(
+		'labels' => array(),
+	), 'register_member_type' );
+
+	$type = (object) $r;
+
+	// Store the post type name as data in the object (not just as the array key).
+	$type->name = $member_type;
+
+	// Make sure the relevant labels have been filled in.
+	$default_name = isset( $r['labels']['name'] ) ? $r['labels']['name'] : ucfirst( $type->name );
+	$r['labels'] = array_merge( array(
+		'name'          => $default_name,
+		'singular_name' => $default_name,
+	), $r['labels'] );
+
+	$bp->members->types[ $member_type ] = $type;
+
+	/**
+	 * Fires after a member type is registered.
+	 *
+	 * @since BuddyPress (2.2.0)
+	 *
+	 * @param string $member_type Member type identifier.
+	 * @param object $type        Member type object.
+	 */
+	do_action( 'bp_registered_member_type', $member_type, $type );
+
+	return $type;
+}
+
+/**
+ * Retrieve a member type object by name.
+ *
+ * @since BuddyPress (2.2.0)
+ *
+ * @param  string $post_type The name of the member type.
+ * @return object A member type object.
+ */
+function bp_get_member_type_object( $member_type ) {
+	$types = bp_get_member_types( array(), 'objects' );
+
+	if ( empty( $types[ $member_type ] ) ) {
+		return null;
+	}
+
+	return $types[ $member_type ];
+}
+
+/**
+ * Get a list of all registered member type objects.
+ *
+ * @since BuddyPress (2.2.0)
+ *
+ * @see bp_register_member_type() for accepted arguments.
+ *
+ * @param array|string $args     Optional. An array of key => value arguments to match against
+ *                               the member type objects. Default empty array.
+ * @param string       $output   Optional. The type of output to return. Accepts 'names'
+ *                               or 'objects'. Default 'names'.
+ * @param string       $operator Optional. The logical operation to perform. 'or' means only one
+ *                               element from the array needs to match; 'and' means all elements
+ *                               must match. Accepts 'or' or 'and'. Default 'and'.
+ * @return array A list of member type names or objects.
+ */
+function bp_get_member_types( $args = array(), $output = 'names', $operator = 'and' ) {
+	$types = buddypress()->members->types;
+
+	$field = 'names' == $output ? 'name' : false;
+
+	$types = wp_filter_object_list( $types, $args, $operator );
+
+	/**
+	 * Filters the array of member type objects.
+	 *
+	 * This filter is run before the $output filter has been applied, so that
+	 * filtering functions have access to the entire member type objects.
+	 *
+	 * @since BuddyPress (2.2.0)
+	 *
+	 * @param array  $types     Member type objects, keyed by name.
+	 * @param array  $args      Array of key=>value arguments for filtering.
+	 * @param string $operator 'or' to match any of $args, 'and' to require all.
+	 */
+	$types = apply_filters( 'bp_get_member_types', $types, $args, $operator );
+
+	if ( 'names' === $output ) {
+		$types = wp_list_pluck( $types, 'name' );
+	}
+
+	return $types;
+}
+
+/**
+ * Set type for a member.
+ *
+ * @since BuddyPress (2.2.0)
+ *
+ * @param int    $user_id     ID of the user.
+ * @param string $member_type Member type.
+ * @param bool   $append      Optional. True to append this to existing types for user,
+ *                            false to replace. Default: false.
+ * @return See {@see bp_set_object_terms()}.
+ */
+function bp_set_member_type( $user_id, $member_type, $append = false ) {
+	// Pass an empty $member_type to remove a user's type.
+	if ( ! empty( $member_type ) && ! bp_get_member_type_object( $member_type ) ) {
+		return false;
+	}
+
+	$retval = bp_set_object_terms( $user_id, $member_type, 'bp_member_type', $append );
+
+	// Bust the cache if the type has been updated.
+	if ( ! is_wp_error( $retval ) ) {
+		wp_cache_delete( $user_id, 'bp_member_type' );
+
+		/**
+		 * Fires just after a user's member type has been changed.
+		 *
+		 * @since BuddyPress (2.2.0)
+		 *
+		 * @param int    $user_id     ID of the user whose member type has been updated.
+		 * @param string $member_type Member type.
+		 * @param bool   $append      Whether the type is being appended to existing types.
+		 */
+		do_action( 'bp_set_member_type', $user_id, $member_type, $append );
+	}
+
+	return $retval;
+}
+
+/**
+ * Get type for a member.
+ *
+ * @since BuddyPress (2.2.0)
+ *
+ * @param  int               $user_id ID of the user.
+ * @param  bool              $single  Optional. Whether to return a single type string. If multiple types are found
+ *                                    for the user, the oldest one will be returned. Default: true.
+ * @return string|array|bool On success, returns a single member type (if $single is true) or an array of member
+ *                           types (if $single is false). Returns false on failure.
+ */
+function bp_get_member_type( $user_id, $single = true ) {
+	$types = wp_cache_get( $user_id, 'bp_member_type' );
+
+	if ( false === $types ) {
+		$types = bp_get_object_terms( $user_id, 'bp_member_type'  );
+
+		if ( ! is_wp_error( $types ) ) {
+			$types = wp_list_pluck( $types, 'name' );
+			wp_cache_set( $user_id, $types, 'bp_member_type' );
+		}
+	}
+
+	$type = false;
+	if ( ! empty( $types ) ) {
+		if ( $single ) {
+			$type = array_pop( $types );
+		} else {
+			$type = $types;
+		}
+	}
+
+	/**
+	 * Filters a user's member type(s).
+	 *
+	 * @since BuddyPress (2.2.0)
+	 *
+	 * @param string $type    Member type.
+	 * @param int    $user_id ID of the user.
+	 * @param bool   $single  Whether to return a single type string, or an array.
+	 */
+	return apply_filters( 'bp_get_member_type', $type, $user_id, $single );
+}
+
+/**
+ * Delete a user's member type when the user when the user is deleted.
+ *
+ * @since BuddyPress (2.2.0)
+ *
+ * @param  int $user_id ID of the user.
+ * @return See {@see bp_set_member_type()}.
+ */
+function bp_remove_member_type_on_user_delete( $user_id ) {
+	return bp_set_member_type( $user_id, '' );
+}
+add_action( 'wpmu_delete_user', 'bp_remove_member_type_on_user_delete' );
+add_action( 'delete_user', 'bp_remove_member_type_on_user_delete' );

commit 01e9aedf085a69a5402267e7afb832e66cee06b6

Fix i18n errors introduced in r9210 and r9226.
`grunt build-commit` has also updated the RTL file for the BP-Legacy template pack.

git-svn-id: https://buddypress.svn.wordpress.org/trunk@9227 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-groups/bp-groups-notifications.php
@@ -40,7 +40,7 @@ function groups_notification_group_updated( $group_id = 0, $old_group = null ) {
 
 		if ( $group->description !== $old_group->description ) {
 			$changed[] = sprintf(
-				__( '* Description changed from "%s" to "%s"', 'Group update email text', 'buddypress' ),
+				_x( '* Description changed from "%s" to "%s"', 'Group update email text', 'buddypress' ),
 				esc_html( $old_group->description ),
 				esc_html( $group->description )
 			);

src/bp-members/bp-members-admin.php
@@ -1030,13 +1030,10 @@ class BP_Members_Admin {
 
 		$types = bp_get_member_types( array(), 'objects' );
 		$current_type = bp_get_member_type( $user->ID );
-
-		/* translators: no option picked in select box */
-		$null_option = __( '----', 'buddypress' );
-
 		?>
+
 		<select name="bp-members-profile-member-type">
-			<option value="" <?php selected( '', $current_type ); ?>><?php esc_attr_e( $null_option ) ?></option>
+			<option value="" <?php selected( '', $current_type ); ?>><?php /* translators: no option picked in select box */ esc_attr_e( '----', 'buddypress' ) ?></option>
 			<?php foreach ( $types as $type ) : ?>
 				<option value="<?php echo esc_attr( $type->name ) ?>" <?php selected( $type->name, $current_type ) ?>><?php echo esc_html( $type->labels['singular_name'] ) ?></option>
 			<?php endforeach; ?>

src/bp-templates/bp-legacy/css/buddypress-rtl.css
@@ -250,7 +250,7 @@ body.activity-permalink #buddypress .activity-list li .activity-header > p {
 }
 #buddypress .activity-list .activity-content .activity-header img.avatar {
 	float: none !important;
-	margin: 0 5px -8px 0 !important;
+	margin: 0 0 -8px 5px !important;
 }
 #buddypress a.bp-secondary-action,
 #buddypress span.highlight {