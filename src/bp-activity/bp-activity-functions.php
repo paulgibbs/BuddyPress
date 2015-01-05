077d74dd6a8d2e5bb207f094ab837624a2a8bd0f
Paul Gibbs <paul@byotos.com>
2015-01-05 09:49:21 +0000

    Fix image summary generation

--- src/bp-activity/bp-activity-functions.php
+++ src/bp-activity/bp-activity-functions.php
@@ -2717,7 +2717,7 @@ function bp_activity_create_summary( $content, $activity ) {
 	if ( $use_media_type === 'embeds' ) {
 		$summary .= PHP_EOL . PHP_EOL . $extracted_media['url'] . PHP_EOL;
 	} elseif ( $use_media_type === 'images' ) {
-		$summary .= $extracted_media['url'];
+		$summary .= sprintf( '<img src="%s">', esc_url( $extracted_media['url'] );
 	}
 
 	return apply_filters( 'bp_activity_create_summary', $summary, $content, $activity );
