aac6dd8f0015bca0c74245758e3a502bd2c9ba5c
Paul Gibbs <paul@byotos.com>
2015-02-10 23:39:37 +0000

    Starting to port p2p_register_connection_type

--- src/bp-core/bp-core-relations.php
+++ src/bp-core/bp-core-relations.php
@@ -10,6 +10,34 @@
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Register a relations connection type.
+ *
+ * @param array $args {
+ *     @type string $name A unique identifier for this connection type.
+ *     @type string|array $from The first end of the connection: post type name or 'user'
+ *     @type array $from_query_vars Additional query vars to pass to WP_Query. Default: none.
+ *     @type string|array $to The second end of the connection: post type name or 'user'
+ *     @type array $to_query_vars Additional query vars to pass to WP_Query. Default: none.
+ *     @type array $fields Metadata fields editable by the user (key=>value pairs). Default: none.
+ *     @type string $cardinality How many connection can each post have:
+ *           'one-to-many', 'many-to-one' or 'many-to-many' (the default).
+ *     @type bool $duplicate_connections Whether to allow more than one connection between the same
+ *           two posts. Default: false.
+ *     @type bool $self_connections Whether to allow a post/user to connect to itself.
+ *           Default: false.
+ *     @type bool $reciprocal For indeterminate connections: True means all connections are
+ *           displayed in a single box. False means 'from' connections are shown in one box
+ *           and 'to' connections are shown in another box. Default: false.
+ * }
+ * @return BP_Relations_Connection_Type|bool Object instance on success, false on failure.
+ * @since BuddyPress (2.3.0)
+ */
+function bp_relations_register_connection_type( Array $args ) {
+	$obj = P2P_Connection_Type_Factory::register( $args );
+	return apply_filters( 'bp_relations_register_connection_type', $ojb );
+}
+
 
 /**
  * Metadata functions.
@@ -104,3 +132,8 @@ function bp_relations_delete_connections_for_type( $objects, $object_type = '' )
 		}
 	}
 }
+
+
+/**
+ * Classes (temp, pending core re-org of core-classes.php)
+ */
