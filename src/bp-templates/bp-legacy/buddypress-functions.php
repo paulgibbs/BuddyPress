paulgibbs/BuddyPress

commit 06fffa2dc4b10dea5f9b8e722dbdb6cd5fe5a8c3

Remove '.min' from asset when locating bp-legacy assets.
This ensures that the assets can be located when running from /src/ when the
minified files have not been built.

Props nofearinc.
Fixes #5963.

git-svn-id: https://buddypress.svn.wordpress.org/trunk@9092 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-templates/bp-legacy/buddypress-functions.php
@@ -323,7 +323,7 @@ class BP_Legacy extends BP_Theme_Compat {
 		$locations['bp-legacy'] = array(
 			'dir'  => bp_get_theme_compat_dir(),
 			'uri'  => bp_get_theme_compat_url(),
-			'file' => $file
+			'file' => str_replace( '.min', '', $file )
 		);
 
 		// Subdirectories within the top-level $locations directories

commit 1dcdf2e05d1726ed07edbd112d364f3395af93c4

BP-Legacy: load minified CSS/JS in all builds except `src`.
Problem was introduced in r9092 which was an attempt to not load minified assets for the `src` build.
This had the unfortunate consequence of not loading the minified assets, anywhere.

Fixes #6213

git-svn-id: https://buddypress.svn.wordpress.org/trunk@9630 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-templates/bp-legacy/buddypress-functions.php
@@ -317,28 +317,32 @@ class BP_Legacy extends BP_Theme_Compat {
 	 *   asset)
 	 */
 	private function locate_asset_in_stack( $file, $type = 'css', $script_handle = '' ) {
-		// Child, parent, theme compat
 		$locations = array();
+
+		// Ensure the assets can be located when running from /src/.
+		if ( defined( 'BP_SOURCE_SUBDIRECTORY' ) && BP_SOURCE_SUBDIRECTORY === 'src' ) {
+			$file = str_replace( '.min', '', $file );
+		}
 
 		// No need to check child if template == stylesheet
 		if ( is_child_theme() ) {
 			$locations['bp-child'] = array(
 				'dir'  => get_stylesheet_directory(),
 				'uri'  => get_stylesheet_directory_uri(),
-				'file' => str_replace( '.min', '', $file )
+				'file' => $file,
 			);
 		}
 
 		$locations['bp-parent'] = array(
 			'dir'  => get_template_directory(),
 			'uri'  => get_template_directory_uri(),
-			'file' => str_replace( '.min', '', $file )
+			'file' => $file,
 		);
 
 		$locations['bp-legacy'] = array(
 			'dir'  => bp_get_theme_compat_dir(),
 			'uri'  => bp_get_theme_compat_url(),
-			'file' => str_replace( '.min', '', $file )
+			'file' => $file,
 		);
 
 		// Subdirectories within the top-level $locations directories