5c68ba4043f9658144ebfb745905492a6dc671fd
Paul Gibbs <paul@byotos.com>
2015-09-22 23:56:24 +0100

    Emails: placeholder code notes for later PHPMailer buildout

--- src/bp-core/classes/class-bp-phpmailer.php
+++ src/bp-core/classes/class-bp-phpmailer.php
@@ -38,7 +38,77 @@ class BP_PHPMailer implements BP_Email_Delivery {
 	 * @param BP_Email $email Email to send.
 	 * @since 2.4.0
 	 */
-		//wp_mail();
-	}
 	public function bp_email( BP_Email $email ) {
+		global $phpmailer;
+
+		// Empty out the values that may be set
+		$phpmailer->ClearAllRecipients();
+		$phpmailer->ClearAttachments();
+		$phpmailer->ClearCustomHeaders();
+		$phpmailer->ClearReplyTos();
+
+		/**
+		 * Filter the email address to send from.
+		 *
+		 * @since 2.2.0
+		 *
+		 * @param string $from_email Email address to send from.
+		 */
+		$phpmailer->From = apply_filters( 'wp_mail_from', $from_email );
+
+		/**
+		 * Filter the name to associate with the "from" email address.
+		 *
+		 * @since 2.3.0
+		 *
+		 * @param string $from_name Name associated with the "from" email address.
+		 */
+		$phpmailer->FromName = apply_filters( 'wp_mail_from_name', $from_name );
+
+		try {
+			$phpmailer->AddAddress( $recipient, $recipient_name);
+		} catch ( phpmailerException $e ) {
+		}
+
+		// Set mail's subject and body
+		$phpmailer->Subject = $subject;
+		$phpmailer->Body    = $message;
+
+		try {
+			$phpmailer->AddCc( $recipient, $recipient_name );
+		} catch ( phpmailerException $e ) {
+			continue;
+		}
+
+		try {
+			$phpmailer->AddBcc( $recipient, $recipient_name );
+		} catch ( phpmailerException $e ) {
+			continue;
+		}
+
+		// Set to use PHP's mail()
+		$phpmailer->IsMail();
+
+		$phpmailer->IsHTML( true );
+		$phpmailer->ContentType = $content_type;
+		$phpmailer->CharSet = apply_filters( 'wp_mail_charset', $charset );
+
+
+		/**
+		 * Fires after PHPMailer is initialized.
+		 *
+		 * @since 2.2.0
+		 *
+		 * @param PHPMailer &$phpmailer The PHPMailer instance, passed by reference.
+		 */
+		do_action_ref_array( 'phpmailer_init', array( &$phpmailer ) );
+
+		// Send!
+		try {
+			return $phpmailer->Send();
+		} catch ( phpmailerException $e ) {
+			return false;
+		}
+
+		}
 }
