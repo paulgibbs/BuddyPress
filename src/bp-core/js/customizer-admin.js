e974b43c2c109371dea612592a104115e56147af
Damian Logghe <info@timersys.com>
2015-10-01 18:36:13 -0300

    updated more mailtpl names

--- src/bp-core/js/customizer-admin.js
+++ src/bp-core/js/customizer-admin.js
@@ -2,21 +2,21 @@
 
     $(window).load(function () {
 
-        wp.customize.panel( 'mailtpl' ).focus();
-        $('.mailtpl_range').on('input',function(){
+        wp.customize.panel( 'bp_mailtpl' ).focus();
+        $('.bp_mailtpl_range').on('input',function(){
             var val = $(this).val();
             $(this).parent().find('.font_value').html(val);
             $(this).val(val);
         });
-        $('#mailtpl-send_mail').on('click', function(e){
+        $('#bp_mailtpl-send_mail').on('click', function(e){
             e.preventDefault();
-            $('#mailtpl-spinner').fadeIn();
+            $('#bp_mailtpl-spinner').fadeIn();
             $.ajax({
                 url     : ajaxurl,
-                data    : { action: 'mailtpl_send_email' }
+                data    : { action: 'bp_mailtpl_send_email' }
             }).done(function(data) {
-                $('#mailtpl-spinner').fadeOut();
-                $('#mailtpl-success').fadeIn().delay(3000).fadeOut();
+                $('#bp_mailtpl-spinner').fadeOut();
+                $('#bp_mailtpl-success').fadeIn().delay(3000).fadeOut();
             });
         });
     });
--- src/bp-core/js/customizer-front.js
+++ src/bp-core/js/customizer-front.js
@@ -1,13 +1,13 @@
 (function( $ ) {
 
 
-    wp.customize( 'mailtpl_opts[body_bg]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[body_bg]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#body' ).css( 'background-color', newval );
         } );
     } );
-    wp.customize( 'mailtpl_opts[header_logo]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[header_logo]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length ) {
                 $( '#logo a' ).html( '<img src="'+newval+'" alt="logo"/>' );
@@ -16,76 +16,76 @@
             }
         } );
     } );
-    wp.customize( 'mailtpl_opts[header_logo_text]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[header_logo_text]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#logo a' ).text( newval );
         } );
     } );
-    wp.customize( 'mailtpl_opts[header_aligment]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[header_aligment]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#logo' ).css( 'text-align', newval );
         } );
     } );
-    wp.customize( 'mailtpl_opts[header_bg]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[header_bg]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#template_header' ).css( 'background-color', newval );
         } );
     } );
-    wp.customize( 'mailtpl_opts[header_text_size]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[header_text_size]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#logo' ).css( 'font-size', newval +'px' );
         } );
     } );
-    wp.customize( 'mailtpl_opts[header_text_color]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[header_text_color]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#logo' ).css( 'color', newval );
         } );
     } );
 
-    wp.customize( 'mailtpl_opts[email_body_bg]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[email_body_bg]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
-                $( '#mailtpl_body_bg' ).css( 'background-color', newval );
+                $( '#bp_mailtpl_body_bg' ).css( 'background-color', newval );
         } );
     } );
-    wp.customize( 'mailtpl_opts[body_text_size]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[body_text_size]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
-                $( '#mailtpl_body' ).css( 'font-size', newval +'px' );
+                $( '#bp_mailtpl_body' ).css( 'font-size', newval +'px' );
         } );
     } );
-    wp.customize( 'mailtpl_opts[body_text_color]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[body_text_color]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
-                $( '#mailtpl_body' ).css( 'color', newval );
+                $( '#bp_mailtpl_body' ).css( 'color', newval );
         } );
     } );
 
-    wp.customize( 'mailtpl_opts[footer_aligment]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[footer_aligment]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#credit' ).css( 'text-align', newval );
         } );
     } );
-    wp.customize( 'mailtpl_opts[footer_bg]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[footer_bg]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#template_footer' ).css( 'background-color', newval );
         } );
     } );
 
-    wp.customize( 'mailtpl_opts[footer_text_size]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[footer_text_size]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#credit' ).css( 'font-size', newval +'px' );
         } );
     } );
-    wp.customize( 'mailtpl_opts[footer_text_color]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[footer_text_color]', function( value ) {
         value.bind( function( newval ) {
             if( newval.length )
                 $( '#credit' ).css( 'color', newval );
@@ -93,13 +93,13 @@
     } );
 
 
-    wp.customize( 'mailtpl_opts[footer_text]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[footer_text]', function( value ) {
         value.bind( function( newval ) {
             $( '#credit' ).html( newval );
         } );
     } );
 
-    wp.customize( 'mailtpl_opts[footer_powered_by]', function( value ) {
+    wp.customize( 'bp_mailtpl_opts[footer_powered_by]', function( value ) {
         value.bind( function( newval ) {
             if( newval == 'off' ) {
                 $( '#powered' ).hide();
