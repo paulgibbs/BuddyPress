8ba08f232a202a4871bb9223aa997a6d3e804c90
Paul Gibbs <paul@byotos.com>
2015-11-15 23:40:16 +0000

    Add todo

--- src/bp-core/admin/js/customizer-admin.js
+++ src/bp-core/admin/js/customizer-admin.js
@@ -2,7 +2,8 @@
 	$( window ).load(function() {
 		wp.customize.panel( 'bp_mailtpl' ).focus();
 
-		// Text size
+		// djpaultodo: move this into a universal/shared file, or move the above into something email-specific.
+		// Range slider
 		$( '.customize-control-range input' ).on( 'input', function() {
 			var val = $( this ).val();
 			$( this ).parent().find( '.range-value' ).html( val );
