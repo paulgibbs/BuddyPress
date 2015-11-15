1a20f094ae249748bfcf05ccfb821333313f197d
Paul Gibbs <paul@byotos.com>
2015-11-15 17:32:31 +0000

    Emails: make range control label better match HTML spec.

--- src/bp-core/classes/class-bp-customizer-control-range.php
+++ src/bp-core/classes/class-bp-customizer-control-range.php
@@ -30,19 +30,18 @@ class BP_Customizer_Control_Range extends WP_Customize_Control {
 		$class = 'customize-control customize-control-' . $this->type;
 
 		?><li id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $class ); ?>">
-			<label>
-				<?php if ( $this->label ) : ?>
+			<?php if ( $this->label ) : ?>
+				<label for="<?php echo esc_attr( "{$id}-range" ); ?>">
 					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-				<?php endif; ?>
+				</label>
+			<?php endif; ?>
 
-				<div class="font-value"><?php echo esc_html( $this->value() ); ?></div>
-				<input type="range" <?php $this->link(); $this->input_attrs(); ?> min="1" max="100" step="1" value="<?php echo esc_attr( $this->value() ); ?>" />
+			<div class="range-value"><?php echo esc_html( $this->value() ); ?></div>
+			<input type="range" id="<?php echo esc_attr( "{$id}-range" ); ?>" <?php $this->link(); $this->input_attrs(); ?> min="1" max="100" step="1" value="<?php echo esc_attr( $this->value() ); ?>" />
 
-				<?php if ( $this->description ) : ?>
-					<p><span class="description customize-control-description"><?php echo esc_html( $this->description ); ?></span></p>
-				<?php endif; ?>
-
-			</label>
+			<?php if ( $this->description ) : ?>
+				<p><span class="description customize-control-description"><?php echo esc_html( $this->description ); ?></span></p>
+			<?php endif; ?>
 		</li><?php
 	}
 }
