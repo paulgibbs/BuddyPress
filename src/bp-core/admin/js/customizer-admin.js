7295fe95c5c87715100e09f4bcf78d4c8fc24312
Paul Gibbs <paul@byotos.com>
2015-11-15 23:12:46 +0000

    Emails, Customiser: remove old JS for send_mail button
    which was removed for now

--- src/bp-core/admin/js/customizer-admin.js
+++ src/bp-core/admin/js/customizer-admin.js
@@ -8,19 +8,5 @@
 			$( this ).parent().find( '.range-value' ).html( val );
 		});
 
-		//
-		$( '#bp-mailtpl-send_mail' ).on( 'click', function( e ) {
-			e.preventDefault();
-			$( '#bp-mailtpl-spinner' ).fadeIn();
-
-			$.ajax({
-				url  : ajaxurl,
-				data : { action: 'bp_mailtpl_send_email' }
-			}).done(function() {
-				$( '#bp-mailtpl-spinner' ).fadeOut();
-				$( '#bp-mailtpl-success' ).fadeIn().delay( 3000 ).fadeOut();
-			});
-		});
-
 	});
 })( jQuery );
