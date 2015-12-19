0505b055dd3c973a0dd8b8282582c557009cf78d
Paul Gibbs <paul@byotos.com>
2015-12-19 20:35:00 +0000

    Emails: use WP charset in new template

--- src/bp-templates/bp-legacy/buddypress/assets/emails/single-bp-email.php
+++ src/bp-templates/bp-legacy/buddypress/assets/emails/single-bp-email.php
@@ -44,7 +44,7 @@ $settings = get_option( 'bp_mailtpl_opts', bp_email_get_customizer_settings_defa
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <meta charset="utf-8"> <!-- utf-8 works for most cases -->
+  <meta charset="<?php echo esc_attr( get_bloginfo( 'charset' ) ); ?>">
 	<meta name="viewport" content="width=device-width"> <!-- Forcing initial-scale shouldn't be necessary -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Use the latest (edge) version of IE rendering engine -->
   <title></title> <!-- The title tag shows in email notifications, like Android 4.4. -->
