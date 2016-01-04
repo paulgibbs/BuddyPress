eb62dba47a1c873de31a54972c80fca569c3a90b
Paul Gibbs <paul@byotos.com>
2016-01-04 00:30:37 +0000

    Customizer: better placeholder/testing values for email template

--- src/bp-templates/bp-legacy/buddypress/assets/emails/single-bp-email.php
+++ src/bp-templates/bp-legacy/buddypress/assets/emails/single-bp-email.php
@@ -133,8 +133,8 @@ $settings = get_option( 'bp_email_options', bp_email_get_customizer_settings_def
 			<!-- Email Header : BEGIN -->
 			<table cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="<?php echo esc_attr( $settings['header_bg'] ); ?>" width="100%" style="max-width: 600px;" class="header_bg">
 			<tr>
-				<img src="http://placehold.it/200x50" width="200" height="50" alt="alt_text" border="0">
 			  <td style="text-align: center; padding: 40px; font-family: sans-serif; mso-height-rule: exactly; color: <?php echo esc_attr( $settings['header_text_color'] ); ?>; font-size: <?php echo esc_attr( $settings['header_text_size'] . 'px' ); ?>" class="header_text_color header_text_size">
+					<?php echo get_bloginfo('name'); ?>
 			  </td>
 			</tr>
 			</table>
@@ -149,9 +149,7 @@ $settings = get_option( 'bp_email_options', bp_email_get_customizer_settings_def
 						<table cellspacing="0" cellpadding="0" border="0" width="100%">
 						  <tr>
 							<td style="padding: 40px; font-family: sans-serif; mso-height-rule: exactly; line-height: <?php echo esc_attr( floor( $settings['body_text_size'] * 1.618 ) . 'px' ) ?>; color: <?php echo esc_attr( $settings['body_text_color'] ); ?>; font-size: <?php echo esc_attr( $settings['body_text_size'] . 'px' ); ?>" class="body_text_color body_text_size">
-								One Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent laoreet malesuada cursus. Maecenas scelerisque congue eros eu posuere. Praesent in felis ut velit pretium lobortis rhoncus ut&nbsp;erat.
-								<br>
-								Two Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent laoreet malesuada cursus. Maecenas scelerisque congue eros eu posuere. Praesent in felis ut velit pretium lobortis rhoncus ut&nbsp;erat.
+								{{content}}
 							</td>
 						  </tr>
 						</table>
