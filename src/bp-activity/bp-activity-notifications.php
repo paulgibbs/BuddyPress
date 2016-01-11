c037c6b2cdad613977333ad837193cf3450d7eae
Paul Gibbs <paul@byotos.com>
2016-01-11 16:59:35 +0000

    Don't render smilies

--- src/bp-activity/bp-activity-notifications.php
+++ src/bp-activity/bp-activity-notifications.php
@@ -117,14 +117,16 @@ function bp_activity_new_comment_notification( $comment_id = 0, $commenter_id = 
 	$poster_name       = bp_core_get_user_displayname( $commenter_id );
 	$thread_link       = bp_activity_get_permalink( $params['activity_id'] );
 
-	remove_filter( 'bp_get_activity_content_body', 'bp_activity_truncate_entry', 5 );
+ 	remove_filter( 'bp_get_activity_content_body', 'convert_smilies' );
 	remove_filter( 'bp_get_activity_content_body', 'wpautop' );
+	remove_filter( 'bp_get_activity_content_body', 'bp_activity_truncate_entry', 5 );
 
 	/** This filter is documented in bp-activity/bp-activity-template.php */
 	$content = apply_filters( 'bp_get_activity_content_body', $params['content'] );
 
-	add_filter( 'bp_get_activity_content_body', 'bp_activity_truncate_entry', 5 );
+ 	add_filter( 'bp_get_activity_content_body', 'convert_smilies' );
 	add_filter( 'bp_get_activity_content_body', 'wpautop' );
+	add_filter( 'bp_get_activity_content_body', 'bp_activity_truncate_entry', 5 );
 
 	if ( $original_activity->user_id != $commenter_id && 'no' != bp_get_user_meta( $original_activity->user_id, 'notification_activity_new_reply', true ) ) {
 		$args = array(
