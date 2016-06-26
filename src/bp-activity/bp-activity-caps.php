37b43bf17aee08f9b68101c3fb1d3ec82d009ecd
Paul Gibbs <djpaul@gmail.com>
2016-06-26 22:08:20 +0100

    Flesh out map meta cap function

--- src/bp-activity/bp-activity-caps.php
+++ src/bp-activity/bp-activity-caps.php
@@ -53,3 +53,32 @@ function bp_activity_get_caps_for_role( $caps, $role ) {
 	return array_merge( $caps, $activity_caps );
 }
 
+/**
+ * Maps Activity capabilities to built-in WordPress capabilities.
+ *
+ * @since 2.7.0
+ *
+ * @param array  $caps    Capabilities for meta capability.
+ * @param string $cap     Capability name.
+ * @param int    $user_id User id.
+ * @param mixed  $args    Arguments.
+ * @return array Actual capabilities for meta capability.
+ */
+function bp_activity_map_meta_caps( $caps, $cap, $user_id, $args ) {
+	switch ( $cap ) {
+	}
+
+
+	/**
+	 * Filter Activity capabilities.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param array  $caps    Capabilities for meta capability.
+	 * @param string $cap     Capability name.
+	 * @param int    $user_id User ID being mapped.
+	 * @param mixed  $args    Capability arguments.
+	 */
+	return apply_filters( 'bp_activity_map_meta_caps', $caps, $cap, $user_id, $args );
+}
+add_filter( 'bp_map_meta_caps', 'bp_activity_map_meta_caps', 10, 4 );
