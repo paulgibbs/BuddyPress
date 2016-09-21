paulgibbs/BuddyPress

commit 49a6e07ae589eeeb38c9f7a2017279fb06598b55

Accessibility: Differentiate navigation landmarks for screen readers.
Fixes #7256.

git-svn-id: https://buddypress.svn.wordpress.org/trunk@11128 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-templates/bp-legacy/buddypress/groups/index.php
@@ -53,7 +53,7 @@ do_action( 'bp_before_directory_groups_page' ); ?>
 		/** This action is documented in bp-templates/bp-legacy/buddypress/activity/index.php */
 		do_action( 'template_notices' ); ?>
 
-		<div class="item-list-tabs" role="navigation">
+		<div class="item-list-tabs" aria-label="<?php esc_attr_e( 'Groups directory main navigation' ); ?>">
 			<ul>
 				<li class="selected" id="groups-all"><a href="<?php bp_groups_directory_permalink(); ?>"><?php printf( __( 'All Groups %s', 'buddypress' ), '<span>' . bp_get_total_group_count() . '</span>' ); ?></a></li>
 
@@ -73,7 +73,7 @@ do_action( 'bp_before_directory_groups_page' ); ?>
 			</ul>
 		</div><!-- .item-list-tabs -->
 
-		<div class="item-list-tabs" id="subnav" role="navigation">
+		<div class="item-list-tabs" id="subnav" aria-label="<?php esc_attr_e( 'Groups directory secondary navigation', 'buddypress' ); ?>" role="navigation">
 			<ul>
 				<?php
 

src/bp-templates/bp-legacy/buddypress/activity/index.php
@@ -41,7 +41,7 @@ do_action( 'bp_before_directory_activity' ); ?>
 	 */
 	do_action( 'template_notices' ); ?>
 
-	<div class="item-list-tabs activity-type-tabs" role="navigation">
+	<div class="item-list-tabs activity-type-tabs" aria-label="<?php esc_attr_e( 'Sitewide activities navigation', 'buddypress' ); ?>" role="navigation">
 		<ul>
 			<?php
 
@@ -137,7 +137,7 @@ do_action( 'bp_before_directory_activity' ); ?>
 		</ul>
 	</div><!-- .item-list-tabs -->
 
-	<div class="item-list-tabs no-ajax" id="subnav" role="navigation">
+	<div class="item-list-tabs no-ajax" id="subnav" aria-label="<?php esc_attr_e( 'Activity secondary navigation', 'buddypress' ); ?>" role="navigation">
 		<ul>
 			<li class="feed"><a href="<?php bp_sitewide_activity_feed_link(); ?>" title="<?php esc_attr_e( 'RSS Feed', 'buddypress' ); ?>"><?php _e( 'RSS', 'buddypress' ); ?></a></li>
 

src/bp-templates/bp-legacy/buddypress/blogs/index.php
@@ -57,7 +57,7 @@ do_action( 'bp_before_directory_blogs_page' ); ?>
 
 	<form action="" method="post" id="blogs-directory-form" class="dir-form">
 
-		<div class="item-list-tabs" role="navigation">
+		<div class="item-list-tabs" aria-label="<?php esc_attr_e( 'Sites directory main navigation', 'buddypress' ); ?>" role="navigation">
 			<ul>
 				<li class="selected" id="blogs-all"><a href="<?php bp_root_domain(); ?>/<?php bp_blogs_root_slug(); ?>"><?php printf( __( 'All Sites %s', 'buddypress' ), '<span>' . bp_get_total_blog_count() . '</span>' ); ?></a></li>
 
@@ -79,7 +79,7 @@ do_action( 'bp_before_directory_blogs_page' ); ?>
 			</ul>
 		</div><!-- .item-list-tabs -->
 
-		<div class="item-list-tabs" id="subnav" role="navigation">
+		<div class="item-list-tabs" id="subnav" aria-label="<?php esc_attr_e( 'Sites directory secondary navigation', 'buddypress' ); ?>" role="navigation">
 			<ul>
 
 				<?php

src/bp-templates/bp-legacy/buddypress/forums/index.php
@@ -55,7 +55,7 @@
 
 	<form action="" method="post" id="forums-directory-form" class="dir-form">
 
-		<div class="item-list-tabs" role="navigation">
+		<div class="item-list-tabs" aria-label="<?php esc_attr_e( 'Forums directory main navigation', 'buddypress' ); ?>" role="navigation">
 			<ul>
 				<li class="selected" id="forums-all"><a href="<?php echo trailingslashit( bp_get_root_domain() . '/' . bp_get_forums_root_slug() ); ?>"><?php printf( __( 'All Topics %s', 'buddypress' ), '<span>' . bp_get_forum_topic_count() . '</span>' ); ?></a></li>
 
@@ -77,7 +77,7 @@
 			</ul>
 		</div>
 
-		<div class="item-list-tabs" id="subnav" role="navigation">
+		<div class="item-list-tabs" id="subnav" aria-label="<?php esc_attr_e( 'Forums secondary navigation', 'buddypress' ); ?>" role="navigation">
 			<ul>
 
 				<?php

src/bp-templates/bp-legacy/buddypress/members/index.php
@@ -56,7 +56,7 @@ do_action( 'bp_before_directory_members_page' ); ?>
 
 	<form action="" method="post" id="members-directory-form" class="dir-form">
 
-		<div class="item-list-tabs" role="navigation">
+		<div class="item-list-tabs" aria-label="<?php esc_attr_e( 'Members directory main navigation', 'buddypress' ); ?>" role="navigation">
 			<ul>
 				<li class="selected" id="members-all"><a href="<?php bp_members_directory_permalink(); ?>"><?php printf( __( 'All Members %s', 'buddypress' ), '<span>' . bp_get_total_member_count() . '</span>' ); ?></a></li>
 
@@ -76,8 +76,7 @@ do_action( 'bp_before_directory_members_page' ); ?>
 			</ul>
 		</div><!-- .item-list-tabs -->
 
-		<div class="item-list-tabs" id="subnav" role="navigation">
-			<ul>
+		<div class="item-list-tabs" id="subnav" aria-label="<?php esc_attr_e( 'Members directory secondary navigation', 'buddypress' ); ?>" role="navigation">
 				<?php
 
 				/**

src/bp-groups/bp-groups-template.php
@@ -4269,7 +4269,7 @@ function bp_groups_get_front_template( $group = null ) {
  */
 function bp_groups_members_template_part() {
 	?>
-	<div class="item-list-tabs" id="subnav" role="navigation">
+	<div class="item-list-tabs" id="subnav" aria-label="<?php esc_attr_e( 'Group secondary navigation', 'buddypress' ); ?>" role="navigation">
 		<ul>
 			<li class="groups-members-search" role="search">
 				<?php bp_directory_members_search_form(); ?>

src/bp-templates/bp-legacy/buddypress/groups/create.php
@@ -35,7 +35,7 @@ do_action( 'bp_before_create_group_page' ); ?>
 		 */
 		do_action( 'bp_before_create_group' ); ?>
 
-		<div class="item-list-tabs no-ajax" id="group-create-tabs" role="navigation">
+		<div class="item-list-tabs no-ajax" id="group-create-tabs">
 			<ul>
 
 				<?php bp_group_creation_tabs(); ?>

src/bp-templates/bp-legacy/buddypress/groups/single/activity.php
@@ -7,7 +7,7 @@
  */
 
 ?>
-<div class="item-list-tabs no-ajax" id="subnav" role="navigation">
+<div class="item-list-tabs no-ajax" id="subnav" aria-label="<?php esc_attr_e( 'Group secondary navigation', 'buddypress' ); ?>" role="navigation">
 	<ul>
 		<li class="feed"><a href="<?php bp_group_activity_feed_link(); ?>" title="<?php esc_attr_e( 'RSS Feed', 'buddypress' ); ?>"><?php _e( 'RSS', 'buddypress' ); ?></a></li>
 

commit 2d5a5ad2b9f0a3c05ac28533f160b43752391290

Specify text domain in string translation function.
Fixes problem with `bp-legacy/groups/index.php` introduced in r11128.

See #7256.

git-svn-id: https://buddypress.svn.wordpress.org/trunk@11137 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-templates/bp-legacy/buddypress/groups/index.php
@@ -53,7 +53,7 @@ do_action( 'bp_before_directory_groups_page' ); ?>
 		/** This action is documented in bp-templates/bp-legacy/buddypress/activity/index.php */
 		do_action( 'template_notices' ); ?>
 
-		<div class="item-list-tabs" aria-label="<?php esc_attr_e( 'Groups directory main navigation' ); ?>">
+		<div class="item-list-tabs" aria-label="<?php esc_attr_e( 'Groups directory main navigation', 'buddypress' ); ?>">
 			<ul>
 				<li class="selected" id="groups-all"><a href="<?php bp_groups_directory_permalink(); ?>"><?php printf( __( 'All Groups %s', 'buddypress' ), '<span>' . bp_get_total_group_count() . '</span>' ); ?></a></li>
 